38fccf4052d82e682b3e0ca475750090f22f7001
Georgy Moiseev <moiseev.georgii@gmail.com>
2024-02-26 17:31:48 +0300

    migrations: add indexed field type change scenario

---
+++ migrations/04_01_change_indexed_type.lua
@@ -0,0 +1,42 @@
+local SPACE_NAME = 'numerical_indexed'
+
+local function migration(...)
+    local SPACE_NAME = ...
+
+    local space = box.space[SPACE_NAME]
+
+    local old_format = space:format()
+    local new_format = table.deepcopy(old_format)
+    new_format[2].type = 'number'
+
+
+    local FUNC_NAME = 'embed'
+
+    if box.schema.func.exists(FUNC_NAME) then
+        box.func[FUNC_NAME]:drop()
+    end
+
+    box.schema.func.create(FUNC_NAME, {
+        language = 'lua',
+        is_deterministic = true,
+        body = [[
+        function(t)
+            local payload = t[2]
+            local new_payload = tonumber(t[2])
+            return {t[1], new_payload}
+        end
+        ]],
+    })
+
+
+    space.index['secondary']:drop()
+
+    space:upgrade({
+        func = FUNC_NAME,
+        format = new_format,
+    })
+
+    space:create_index('secondary', {parts = {'val'}, if_not_exists = true})
+end
+
+return require('migrations.run_and_introspect')(migration, SPACE_NAME)
